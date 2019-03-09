a01c2f1d69909558a1ded2e959cebb35146c7fbc
MarinaSMS <17121981mar@gmail.com>
2019-03-09 20:06:49 +0300

    site2

---
+++ site2/precache-manifest.aa5ff5b01c9be98aa3e01684e509d8cb.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "e02f59c49d051994c4d1",
+    "url": "/static/css/main.50ef5a85.chunk.css"
+  },
+  {
+    "revision": "e02f59c49d051994c4d1",
+    "url": "/static/js/main.e02f59c4.chunk.js"
+  },
+  {
+    "revision": "37be3cda631070632fc3",
+    "url": "/static/js/1.37be3cda.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e6810b7ca389be5eac15141063c793c4",
+    "url": "/static/media/apelsin2.e6810b7c.png"
+  },
+  {
+    "revision": "867d41cbf9d1975cd8a64747f08dd1f2",
+    "url": "/static/media/logo.867d41cb.svg"
+  },
+  {
+    "revision": "157be2d266d3fa407a1979a596f18694",
+    "url": "/static/media/exit.157be2d2.svg"
+  },
+  {
+    "revision": "efedcf585f80097401cefa47bfcd7b2f",
+    "url": "/static/media/thumbs-up.efedcf58.svg"
+  },
+  {
+    "revision": "e93e49cab6ee2f2678dd9c524f08b5ce",
+    "url": "/static/media/message.e93e49ca.svg"
+  },
+  {
+    "revision": "7008bf300375817c5ab9cccb61f1849b",
+    "url": "/static/media/newss.7008bf30.svg"
+  },
+  {
+    "revision": "21e73793da4382200692ad59b4b323dd",
+    "url": "/static/media/music.21e73793.svg"
+  },
+  {
+    "revision": "af3d80aa447ffdec28f68b697f0eebeb",
+    "url": "/static/media/settings.af3d80aa.svg"
+  },
+  {
+    "revision": "761c730077a50fb3b2fe6c5d6221dbfe",
+    "url": "/static/media/camera .761c7300.svg"
+  },
+  {
+    "revision": "2d4420c8c1840cf4edd562325d097b6d",
+    "url": "/static/media/users.2d4420c8.svg"
+  },
+  {
+    "revision": "e77016ce44e400bb9e40e434216f95cf",
+    "url": "/static/media/skinali.e77016ce.jpg"
+  },
+  {
+    "revision": "58f65bc149a12b25339596d1b2e39f6e",
+    "url": "/index.html"
+  }
+];
